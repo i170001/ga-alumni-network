08049fce6486d70a2508943cb750614e59a0ef17
conanjip <39581203+conanjip@users.noreply.github.com>
2024-07-04 19:34:59 +0800

    feature/setup-mongoose-connection-v2

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 require("dotenv").config();
 require("./client/users");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var jobsRouter = require('./routes/jobs');
 
 var app = express();
 
@@ -16,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/jobs', jobsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
