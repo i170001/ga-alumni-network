ab9854acdac9cce49f135175f8e3b34e3d0f2a70
conanjip <39581203+conanjip@users.noreply.github.com>
2024-07-06 16:39:46 +0800

    feature/cloudinary-intergration-v1

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -5,7 +5,7 @@ var jobsCtrl = require('../controllers/jobs');
 /* GET Jobs data (all users) */
 router.get("/", jobsCtrl.getJobs);
 
-/* POST Job data (signed in user) */
+/* POST Job data (create post by user) */
 router.post("/createjob", jobsCtrl.createJob);
 
 module.exports = router;
