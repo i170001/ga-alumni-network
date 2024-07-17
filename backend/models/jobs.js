0557cf45da8b574a0ec074f6b0429c1b35d4931e
conanjip <39581203+conanjip@users.noreply.github.com>
2024-07-18 03:25:59 +0800

    improvement/job-crud-setup-v1

--- backend/models/jobs.js
+++ backend/models/jobs.js
@@ -1,44 +1,50 @@
-const jobsDao = require("../daos/jobs")
+const jobsDao = require("../daos/jobs");
 
 module.exports = {
-    getJobs,
-    createJob,
-    findOneAndUpdate,
-    findOneAndDelete,
-  };
+  getJobs,
+  createJob,
+  findOneAndUpdate,
+  findOneAndDelete,
+  getUserJobs, // Add this line
+};
 
 function getJobs(queryFields) {
-    return jobsDao.find(queryFields);
+  return jobsDao.find(queryFields);
 }
 
 async function createJob(body) {
-  console.log("Model Request Body:", body); 
+  console.log("Model Request Body:", body);
 
-  const data = await jobsDao.findOne({"listing_id": body.listing_id});
+  const data = await jobsDao.findOne({ listing_id: body.listing_id });
   console.log(data);
   if (data) {
-    return {success: false, error: "job already exist"};
+    return { success: false, error: "Job already exists" };
   }
-  const newUser = await jobsDao.create(body);
-  return {success: true, data: newUser};
+  const newJob = await jobsDao.create(body);
+  return { success: true, data: newJob };
 }
 
 async function findOneAndUpdate(filter, update, options) {
   try {
-      const updatedJob = await jobsDao.findOneAndUpdate(filter, update, options);
-      return updatedJob;
+    const updatedJob = await jobsDao.findOneAndUpdate(filter, update, options);
+    return updatedJob;
   } catch (error) {
-      console.error("Error updating job:", error);
-      throw error; // Rethrow the error to be caught by the controller
+    console.error("Error updating job:", error);
+    throw error; // Rethrow the error to be caught by the controller
   }
 }
 
 async function findOneAndDelete(filter, options) {
   try {
-      const deletedJob = await jobsDao.findOneAndDelete(filter, options);
-      return deletedJob;
+    const deletedJob = await jobsDao.findOneAndDelete(filter, options);
+    return deletedJob;
   } catch (error) {
-      console.error("Error deleting job:", error);
-      throw error; // Rethrow the error to be caught by the controller
+    console.error("Error deleting job:", error);
+    throw error; // Rethrow the error to be caught by the controller
   }
+}
+
+// New function to get jobs by user_id
+async function getUserJobs(user_id) {
+  return jobsDao.find({ user_id });
 }
